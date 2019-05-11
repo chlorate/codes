7e370a2427bd346e9096494d35e1c5d8c8fda066
Chlorate <me@chlorate.ca>
2019-05-11 16:30:47 -0300

    Fix conflicts with registers Gecko uses
    Crashed when using other codes with retry checkpoint.

--- src/mega-man-9/retry-checkpoint.asm
+++ src/mega-man-9/retry-checkpoint.asm
@@ -12,30 +12,30 @@
 .set mode,  0x80314100
 
 # Registers:
-# - r5: retryState address (high halfword)
-# - r6: retryState value
-# - r7: mode address (high halfword)
-# - r8: mode value
+# - r9: retryState address (high halfword)
+# - r10: retryState value
+# - r11: mode address (high halfword)
+# - r12: mode value
 
-  lis r5, state@h                        # \ Read code state
-  lbz r6, state@l (r5)                   # /
-  lis r7, mode@h                         # \ Read game mode
-  lhz r8, mode@l (r7)                    # /
-  lis r3, input@h+0x10000                # \ Read Classic Controller input
-  lhz r3, input@l-0x10000 (r3)           # / (optimization: one less instruction)
+  lis r9, state@h                          # \ Read code state
+  lbz r10, state@l (r9)                    # /
+  lis r11, mode@h                          # \ Read game mode
+  lhz r12, mode@l (r11)                    # /
+  lis r3, input@h+0x10000                  # \ Read Classic Controller input
+  lhz r3, input@l-0x10000 (r3)             # / (optimization: one less instruction)
 
 # Input check: Activate when the button combo is pressed and then don't activate
 # again until the button combo is released. This avoids rapidly activating when
 # the button combo is held.
 
-  cmplwi r3, buttonCombo                 # \ if Select + A is not pressed:
-  beq pressed                            # |   Clear pressed bit
-  andi. r6, r6, ~pressedBit@l            # |
-  b activation                           # /
-pressed:                                 # \ else if pressed bit is cleared:
-  andi. r0, r6, pressedBit               # |   Set pressed and activated bits
-  bgt activation                         # |
-  ori r6, r6, pressedBit | activatedBit  # /
+  cmplwi r3, buttonCombo                   # \ if Select + A is not pressed:
+  beq pressed                              # |   Clear pressed bit
+  andi. r10, r10, ~pressedBit@l            # |
+  b activation                             # /
+pressed:                                   # \ else if pressed bit is cleared:
+  andi. r0, r10, pressedBit                # |   Set pressed and activated bits
+  bgt activation                           # |
+  ori r10, r10, pressedBit | activatedBit  # /
 
 # Activation: When the activated bit is set, a respawn will be triggered when it
 # is safe to do so.
@@ -52,31 +52,31 @@ pressed:                                 # \ else if pressed bit is cleared:
 # respawning.
 
 activation:
-  andi. r0, r6, activatedBit             # \ if activated bit is cleared:
-  beq end                                # /   Do nothing
-  cmplwi r8, 0x0009                      # \ else if mode == first stage fade-in frame:
-  beq deactivate                         # /   Deactivate, write mode (unchanged)
-  cmplwi r8, 0x000a                      # \ else if mode == normal gameplay:
-  beq openPauseMenu                      # /   Open pause menu, write mode
-  cmplwi r8, 0x0100                      # \ else if mode == first pause menu fade-in frame:
-  beq respawn                            # /   Respawn, deactivate, write mode
-  cmplwi r8, 0x0101                      # \ else if mode == pause menu:
-  beq respawn                            # /   Respawn, deactivate, write mode
-  cmplwi r8, 0x0a01                      # \ else if mode == challenge menu:
-  beq respawn                            # /   Respawn, deactivate, write mode
-  b end                                  # > else: Do nothing
-openPauseMenu:                           # \ Open pause menu:
-  li r8, 0x000b                          # |   Set mode to fade to pause menu
-  b writeMode                            # /   (+ write mode)
-respawn:                                 # \ Respawn:
-  li r8, 0x0008                          # |   Set mode to respawn at last checkpoint
-                                         # /   (+ fall through to deactivate)
-deactivate:                              # \ Deactivate:
-  andi. r6, r6, ~activatedBit@l          # |   Clear activated bit
-                                         # /   (+ fall through to write mode)
-writeMode:                               # \ Write mode:
-  sth r8, mode@l (r7)                    # /   Write new mode
+  andi. r0, r10, activatedBit              # \ if activated bit is cleared:
+  beq end                                  # /   Do nothing
+  cmplwi r12, 0x0009                       # \ else if mode == first stage fade-in frame:
+  beq deactivate                           # /   Deactivate, write mode (unchanged)
+  cmplwi r12, 0x000a                       # \ else if mode == normal gameplay:
+  beq openPauseMenu                        # /   Open pause menu, write mode
+  cmplwi r12, 0x0100                       # \ else if mode == first pause menu fade-in frame:
+  beq respawn                              # /   Respawn, deactivate, write mode
+  cmplwi r12, 0x0101                       # \ else if mode == pause menu:
+  beq respawn                              # /   Respawn, deactivate, write mode
+  cmplwi r12, 0x0a01                       # \ else if mode == challenge menu:
+  beq respawn                              # /   Respawn, deactivate, write mode
+  b end                                    # > else: Do nothing
+openPauseMenu:                             # \ Open pause menu:
+  li r12, 0x000b                           # |   Set mode to fade to pause menu
+  b writeMode                              # /   (+ write mode)
+respawn:                                   # \ Respawn:
+  li r12, 0x0008                           # |   Set mode to respawn at last checkpoint
+                                           # /   (+ fall through to deactivate)
+deactivate:                                # \ Deactivate:
+  andi. r10, r10, ~activatedBit@l          # |   Clear activated bit
+                                           # /   (+ fall through to write mode)
+writeMode:                                 # \ Write mode:
+  sth r12, mode@l (r11)                    # /   Write new mode
 
 end:
-  stb r6, state@l (r5)                   # > Write new code state
+  stb r10, state@l (r9)                    # > Write new code state
   blr
