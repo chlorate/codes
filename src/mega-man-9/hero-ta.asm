197070869dd62aa8f1df916e41e3ebaa7fc89139
Chlorate <me@chlorate.ca>
2019-05-12 23:12:49 -0300

    Add MM9 Hero and Superhero TA codes

---
+++ src/mega-man-9/hero-ta.asm
@@ -0,0 +1,3 @@
+# Inject at: 0x80089828 (same for both WR9E and WR9P)
+
+li r0, 0x0001  # > Use Hero mode (except in Special Stage)
---
+++ src/mega-man-9/superhero-ta.asm
@@ -0,0 +1,3 @@
+# Inject at: 0x80089828 (same for both WR9E and WR9P)
+
+li r0, 0x0002  # > Use Superhero mode (except in Special Stage)
