47c4abe594b0764987426dd77c2b7837228f9f07
Chlorate <me@chlorate.ca>
2021-05-16 20:53:31 -0300

    Refactor game state logic into separate code

--- src/mega-man-10/exit-to-stage-select.asm
+++ src/mega-man-10/exit-to-stage-select.asm
@@ -1,146 +1,44 @@
-.set codeState,    0x81000000
-.set pressedBit,   0b00010000
-.set activatedBit, 0b00100000
+.set version, 'E'
+
+.set pressedState, 0x81000000
+.set pressedBit,   0b00000100
 .set buttonCombo,  0b0001000000100000  # Select + Y
 
-# WRXE addresses:
-.set input,     0x804534ea
-.set gameState, 0x805336e8
-.set menuState, 0x80fb9000
+.set changerState, 0x81000002
+.set changerBits,  0b0000000000000010  # All game states except Stage Select
+.set newGameState, 0x0001
 
-.set stageSelectState, gameState + 0x1244
-.set gameMode,         gameState + 0x22ff
-.set weaponMenuState,  menuState + 0x0039
+.if version == 'E'
+  .set input,    0x804534ea
+  .set gameMode, 0x805359e7
+.endif
 
-# Registers:
-# - r9: code state address (high halfword)
-# - r10: code state value
-# - r11: game state address (high halfword)
-# - r12: game state value (high halfword)
-# - r13: game state value (low halfword)
-# - r14: menu state address (high halfword)
+  lis r3, gameMode@h             # \ Read game mode
+  lbz r3, gameMode@l (r3)        # /
+  cmplwi r3, 0x0001              # \ Do nothing if not playing main game
+  bgt end                        # /
 
-  lis r11, gameState@h                     # \ Read game mode
-  lbz r3, gameMode@l (r3)                  # /
-  cmplwi r3, 0x0000                        # \ if not playing main game:
-  bne end                                  # /   Do nothing
+  lis r9, pressedState@h         # \ Read pressed state
+  lbz r10, pressedState@l (r9)   # /
 
-  lis r9, codeState@h                      # \ Read code state
-  lbz r10, codeState@l (r9)                # /
+  lis r3, input@h                # \ Read Classic Controller input
+  lhz r3, input@l (r3)           # /
+  cmplwi r3, buttonCombo         # \ Clear pressed bit if button combo is
+  beq pressed                    # | not pressed
+  andi. r10, r10, ~pressedBit@l  # |
+  b writePressed                 # /
 
-  lis r3, input@h                          # \ Read Classic Controller input
-  lhz r3, input@l (r3)                     # /
-  cmplwi r3, buttonCombo                   # \ if Select + Y is not pressed:
-  beq pressed                              # |   Clear pressed bit
-  andi. r10, r10, ~pressedBit@l            # |
-  b activation                             # /
 pressed:
-  andi. r0, r10, pressedBit                # \ if Select + Y is pressed
-  bgt activation                           # | and pressed bit is cleared:
-  ori r10, r10, pressedBit | activatedBit  # /   Set pressed and activated bits
+  andi. r0, r10, pressedBit      # > Check if already pressed
+  bgt end
+  lis r5, changerBits            # \ Write payload for game state changer code
+  ori r5, r5, newGameState       # |
+  lis r3, changerState@h         # |
+  stw r5, changerState@l (r3)    # /
+  ori r10, r10, pressedBit       # > Set pressed bit
 
-activation:
-  andi. r0, r10, activatedBit              # \ if not activated:
-  beq writeCodeState                       # /   Do nothing
-  lis r14, menuState@h+0x10000
-  lhz r12, gameState@l (r11)               # \ Read game state
-  lhz r13, gameState@l+0x0002 (r11)        # /
-  cmplwi r12, 0x000a                       # \ if normal gameplay:
-  beq openWeaponMenu                       # /   Open weapon menu
-  cmplwi r12, 0x000b                       # \ if fade out gameplay to menu:
-  beq openWeaponMenu                       # |   Force weapon menu to open (avoid seeing
-                                           # /   one frame of pause menu fade in)
-  cmplwi r12, 0x000c                       # \ if fade in gameplay from menu:
-  beq checkFadeState                       # /   Need to check low halfword of game state
-  cmplwi r12, 0x0101                       # \ if weapon menu:
-  beq closeWeaponMenu                      # /   Close weapon menu
-  cmplwi r12, 0x0200                       # \ if stage select:
-  beq deactivate                           # /   Deactivate
-  cmplwi r12, 0x0401                       # \ if boss intro cutscene:
-  beq endBossIntro                         # /   End cutscene
-  cmplwi r12, 0x0601                       # \ if get weapon cutscene:
-  beq endGetWeapon                         # /   End cutscene
-  cmplwi r12, 0x0805                       # \ if game/save/load menu:
-  beq checkGameMenus                       # /   Need to check low halfword of game state
-  cmplwi r12, 0x0a01                       # \ if in challenge menu:
-  beq deactivate                           # /   Deactivate
-  cmplwi r12, 0x0c00                       # \ if initializing pause menu:
-  beq writeCodeState                       # |   Do nothing (triggering now would go to
-                                           # /   stage select instead)
-  cmplwi r12, 0x0c01                       # \ if pause menu:
-  beq closePauseMenu                       # /   Close pause menu
-  cmplwi r12, 0x1001                       # \ if options menu:
-  beq deactivate                           # /   Deactivate
-  b exitToStageSelect
-
-checkFadeState:
-  andi. r0, r13, 0x00ff                    # \ if first black frame after closing menu:
-  beq exitToStageSelect                    # /   Exit to stage select
-  b openWeaponMenu
-
-checkGameMenus:
-  li r5, 0x000c                            # \ if game menu via game over:
-  li r7, 0x000b                            # |   Try closing game menu
-  cmplwi r13, 0x0000                       # |
-  beq closeGameMenu                        # /
-  li r5, 0x000a                            # \ if game menu via stage select:
-  li r7, 0x000a                            # |   Try closing game menu
-  cmplwi r13, 0x0100                       # |
-  beq closeGameMenu                        # /
-  cmplwi r13, 0x0401                       # \ if load menu:
-  beq deactivate                           # /   Deactivate
-  cmplwi r13, 0x0601                       # \ if save menu:
-  beq deactivate                           # /   Deactivate
-  b writeCodeState
-
-openWeaponMenu:
-  li r12, 0x000b                           # \ Open weapon menu or switch to weapon menu
-  andi. r13, r13, 0x00ff                   # / if any menu is currently opening
-  b writeGameState
-
-closeWeaponMenu:
-  lbz r3, weaponMenuState@l-0x10000 (r14)
-  cmplwi r3, 0x0002                        # \ if weapon menu not closing:
-  beq writeCodeState                       # |   Close the menu
-  li r3, 0x0200                            # |
-  sth r3, weaponMenuState@l-0x10000 (r14)  # /
-  b writeCodeState
-
-closePauseMenu:
-  li r13, 0x0400
-  b writeGameState
-
-closeGameMenu:
-  # r5 = menu fade out state value
-  # r7 = menu state value for closing the menu
-  lbz r3, menuState@l-0x10000 (r14)
-  cmplwi r3, 0x0000                        # \ if game menu is initializing:
-  beq writeCodeState                       # /   Do nothing (goes to stage select otherwise)
-  cmplw r3, r5                             # \ if fading out:
-  beq writeCodeState                       # /   Do nothing (freezes game over menu otherwise)
-  stb r7, menuState@l-0x10000 (r14)        # > Close game menu
-  b writeCodeState
-
-endBossIntro:
-  li r13, 0x0900
-  b writeGameState
-
-endGetWeapon:
-  li r13, 0x0600
-  b writeGameState
-
-exitToStageSelect:
-  li r12, 0x0001                           # > Set game state to stage select
-
-deactivate:
-  andi. r10, r10, ~activatedBit@l          # > Clear activated bit
-
-writeGameState:
-  sth r12, gameState@l (r11)               # \ Write new game state
-  sth r13, gameState@l+0x0002 (r11)        # /
-
-writeCodeState:
-  stb r10, codeState@l (r9)                # > Write new code state
+writePressed:
+  stb r10, pressedState@l (r9)   # > Write new pressed state
 
 end:
   blr
--- src/mega-man-10/retry-checkpoint.asm
+++ src/mega-man-10/retry-checkpoint.asm
@@ -1,147 +1,38 @@
-.set state,        0x81000002
-.set pressedBit,   0b00000001
-.set activatedBit, 0b00000010
+.set version, 'E'
+
+.set pressedState, 0x81000000
+.set pressedBit,   0b00010000
 .set buttonCombo,  0b0001000000010000  # Select + A
 
-# WRXE addresses:
-.set input,               0x804534ea
-.set gameMode,            0x805336e8
-.set gameType,            0x805359e7
-.set weaponMenuMode,      0x80fb9039
-.set currentRefightKills, 0x80510661
-.set initialRefightKills, 0x80535b0d
-.set objectSpawns,        0x805c6b4c
+.set changerState, 0x81000002
+.set changerBits,  0b0000000000000000  # Gameplay states only
+.set newGameState, 0x0008
 
-# Registers:
-# - r9: state address (high halfword)
-# - r10: state value
-# - r11: game mode address (high halfword)
-# - r12: game mode value (high halfword)
-# - r13: game mode value (low halfword)
+.if version == 'E'
+  .set input, 0x804534ea
+.endif
 
-  lis r9, state@h                          # \ Read code state
-  lbz r10, state@l (r9)                    # /
+  lis r9, pressedState@h         # \ Read pressed state
+  lbz r10, pressedState@l (r9)   # /
 
-# Input check: Activate when the button combo is pressed and then don't activate
-# again until the button combo is released. This avoids rapidly activating when
-# the button combo is held.
+  lis r3, input@h                # \ Read Classic Controller input
+  lhz r3, input@l (r3)           # /
+  cmplwi r3, buttonCombo         # \ Clear pressed bit if button combo is
+  beq pressed                    # | not pressed
+  andi. r10, r10, ~pressedBit@l  # |
+  b writePressed                 # /
 
-  lis r3, input@h                          # \ Read Classic Controller input
-  lhz r3, input@l (r3)                     # /
-  cmplwi r3, buttonCombo                   # \ if Select + A is not pressed:
-  beq pressed                              # |   Clear pressed bit
-  andi. r10, r10, ~pressedBit@l            # |
-  b activation                             # /
 pressed:
-  andi. r0, r10, pressedBit                # \ if Select + A is pressed
-  bgt activation                           # | and pressed bit is cleared:
-  ori r10, r10, pressedBit | activatedBit  # /   Set pressed and activated bits
+  andi. r0, r10, pressedBit      # > Check if already pressed
+  bgt end
+  lis r5, changerBits            # \ Write payload for game state changer code
+  ori r5, r5, newGameState       # |
+  lis r3, changerState@h         # |
+  stw r5, changerState@l (r3)    # /
+  ori r10, r10, pressedBit       # > Set pressed bit
 
-# Activation: When the activated bit is set, a respawn will be triggered when it
-# is safe to do so.
-#
-# There is one main "game mode" value in memory that controls the execution of
-# normal gameplay, menus, or other functions, including respawning. There are
-# some secondary values that represent the current state of certain menus.
-#
-# The game allows respawning any time after starting a new game (e.g., during
-# stage select or boss intros). With MM9, there were some edge cases where
-# respawning crashes the game. I'm assuming this is the same for MM10. The
-# safest spots to respawn are the black screens when transitioning between
-# gameplay and menus. This code opens and closes menus as necessary.
-
-activation:
-  andi. r0, r10, activatedBit              # \ if not activated:
-  beq end                                  # /   Do nothing
-  lis r3, gameType@h                       # \ Read game type
-  lbz r3, gameType@l (r3)                  # /
-  cmplwi r3, 0x0005                        # \ if playing TA replay:
-  beq deactivate                           # /   Deactivate
-  lis r11, gameMode@h                      # \ Read game mode
-  lhz r12, gameMode@l (r11)                # |
-  lhz r13, gameMode@l+0x0002 (r11)         # /
-  cmplwi r12, 0x0009                       # \ if first stage fade-in frame:
-  beq deactivate                           # /   Deactivate
-  cmplwi r12, 0x000a                       # \ if normal gameplay:
-  beq openWeaponMenu                       # /   Open weapon menu
-  cmplwi r12, 0x000b                       # \ if fade out gameplay to menu:
-  beq forceWeaponMenu                      # /   Force weapon menu to open
-  cmplwi r12, 0x000c                       # \ if fade in gameplay from menu:
-  beq checkFadeMode                        # /   Need to check low halfword of game mode
-  cmplwi r12, 0x0100                       # \ if fade in weapon menu from gameplay:
-  beq respawn                              # /   Respawn
-  cmplwi r12, 0x0101                       # \ if in weapon menu:
-  beq checkWeaponMenuMode                  # /   Need to check weapon menu mode
-  cmplwi r12, 0x0a01                       # \ if in challenge menu:
-  beq deactivate                           # /   Deactivate
-  cmplwi r12, 0x0c01                       # \ if in pause menu:
-  beq checkPauseMenuMode                   # /   Need to check low halfword of game mode
-  cmplwi r12, 0x1001                       # \ if in option menu:
-  beq deactivate                           # /   Deactivate
-  b end
-
-checkFadeMode:
-  andi. r0, r13, 0x00ff                    # \ if first black frame after closing menu:
-  beq respawn                              # /   Respawn
-  b end
-
-checkPauseMenuMode:
-  cmplwi r13, 0x0300                       # \ if pause menu active:
-  beq closePauseMenu                       # /   Close pause menu
-  cmplwi r13, 0x0303                       # \ if retry/exit confirmation active:
-  beq closePauseMenu                       # /   Close pause menu
-  b end
-closePauseMenu:
-  li r13, 0x0400                           # > Set game mode to close pause menu
-  b writeGameMode
-
-checkWeaponMenuMode:
-  lis r3, weaponMenuMode@h+0x10000         # \ Read weapon menu mode
-  lhz r5, weaponMenuMode@l-0x10000 (r3)    # / (optimization: one less instruction)
-  cmplwi r5, 0x0101                        # \ if weapon menu active:
-  bne end                                  # |   Set weapon menu mode to close
-  li r5, 0x0200                            # |   the menu
-  sth r5, weaponMenuMode@l-0x10000 (r3)    # /
-  b end
-
-# There is some weirdness with the pause menu where triggering a respawn on
-# a specific frame (second frame when r12 == 0x0c00) causes the game to go to
-# the stage select instead. It's because Gecko codes run in the middle of
-# a pause menu routine for some reason, and the rest of the routine sets one
-# byte of the game mode, changing it from 0x0008 to 0x0001 which is the stage
-# select mode. The weapon menu doesn't have this problem.
-
-forceWeaponMenu:
-  cmplwi r13, 0x0101                       # \ if middle of fade from gameplay to pause menu:
-  bne end                                  # |   Set mode to open weapon menu instead
-  li r13, 0x0001                           # /
-  b writeGameMode
-
-openWeaponMenu:
-  li r12, 0x000b                           # > Set game mode to open weapon menu
-  b writeGameMode
-
-respawn:
-  li r0, -0x0001                           # \ Set all stage object spawn bits
-  lis r3, objectSpawns@h                   # | (in particular, to respawn energy pickups)
-  ori r3, r3, objectSpawns@l               # |
-  li r5, 0x007c                            # |
-loopRespawnObjects:                        # |
-  stwx r0, r3, r5                          # |
-  subic. r5, r5, 0x0004                    # |
-  bge loopRespawnObjects                   # /
-  lis r3, currentRefightKills@h            # \ Save current boss refight kill state
-  lbz r3, currentRefightKills@l (r3)       # |
-  stb r3, initialRefightKills@l (r11)      # / (optimization: r11 uses same high halfword)
-  li r12, 0x0008                           # > Set game mode to respawn at checkpoint
-
-deactivate:
-  andi. r10, r10, ~activatedBit@l          # > Clear activated bit
-
-writeGameMode:
-  sth r12, gameMode@l (r11)                # \ Write new game mode
-  sth r13, gameMode@l+0x0002 (r11)         # /
+writePressed:
+  stb r10, pressedState@l (r9)   # > Write new pressed state
 
 end:
-  stb r10, state@l (r9)                    # > Write new code state
   blr
